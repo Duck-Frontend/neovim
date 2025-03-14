427943bd2dd8a5da632ae7b88fcd5725389701fb
Anton Stadolnik <stadolnikanton1@gmail.com>
2025-03-15 00:06:45 +0300

    Импортирование файлов настроек в основной файл конфигурации

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Подключение основных настроек
+require('core.options')
+require('core.keymaps')
+require('core.autocmds')
+
+-- Подключение настроек плагинов
+require('plugins.init')
+
+-- Подключение цветовой схемы
+require('colorscheme')
