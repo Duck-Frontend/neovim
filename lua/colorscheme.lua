b4bc58dbf371bdd9efda101c493afa4d06aa7471
Anton Stadolnik <stadolnikanton1@gmail.com>
2025-03-15 21:04:09 +0300

    Настройка прозрачности

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,10 @@
--- Для Lua (init.lua)
 require('catppuccin').setup({
   flavour = 'mocha', -- latte, frappe, macchiato, mocha
+  transparent_background = true, -- Прозрачность
+  styles = {
+    comments = { 'italic' }, -- Стиль комментариев
+    functions = { 'bold' }, -- Стиль функций
+    keywords = { 'italic' }, -- Стиль ключевых слов
+  },
 })
 vim.cmd('colorscheme catppuccin')
